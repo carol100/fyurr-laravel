a4b71a0bb84c1ec852f5b4c12c972cec7b5be73d
Alvin Wanjohi <akwanjohi99@gmail.com>
2022-10-18 18:08:42 +0300

    Added custom error pages.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+  <h1>Sorry ...</h1>
+  <p>There's nothing here!</p>
+  <p><a href="{{ route('home') }}">Back</a></p>  
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('artists', ArtistController::class);
 
